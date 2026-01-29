94fb88c91b2e3ad46a3585af3cbfa362c841b11b
gojescore <apbeyer@hotmail.com>
2026-01-29 17:10:11 +0100

    Update constants.js

--- server/shared/constants.js
+++ server/shared/constants.js
@@ -3,7 +3,10 @@ module.exports = {
   MONEY_WRONG_PENALTY: 100,
   MONEY_PICKUP_AMOUNT: 100,
   MONEY_PICKUPS_PER_CORRECT: 1,
-  MONEY_PICKUP_RADIUS: 18,    // how close a player must be to pick it up
-  MONEY_PICKUP_TTL_MS: 25000, // despawn after 25s
-  MONEY_ROAD_SPAWN_TRIES: 50, // tries to find a valid road point
+  MONEY_PICKUP_RADIUS: 18,
+  MONEY_PICKUP_TTL_MS: 25000,
+  MONEY_ROAD_SPAWN_TRIES: 50,
+
+  // NEW
+  MAX_NONPERM_SLOTS: 3,
 };
